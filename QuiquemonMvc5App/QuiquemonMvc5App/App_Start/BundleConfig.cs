e8ae38b7ce01b600cc77ddc3b4e177f12409a656
PROGRAMACION2 <PROGRAMACION2@LAPTOP-PT7LJ4TU>
2017-03-16 16:15:15 -0600

    Set up JQuery, JQuery Validation, Bootstrap,  BundleConfig. HomeController and its 3 actions created and filled.

---
+++ QuiquemonMvc5App/QuiquemonMvc5App/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Optimization;
+
+namespace QuiquemonMvc5App
+{
+	public class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+				"~/Scripts/jquery-3.1.1.min.js",
+				"~/Scripts/jquery.validate.min.js"
+			));
+			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+				"~/Scripts/bootstrap.min.js",
+				"~/Scripts/bootstrap-dialog.js"
+			));
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+				"~/Content/bootstrap.min.css",
+				"~/Content/bootstrap-theme.min.css",
+				"~/Content/bootstrap-dialog.css"
+			));
+		}
+	}
+}
---
+++ QuiquemonMvc5App/QuiquemonMvc5App/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuiquemonMvc5App.Controllers
+{
+	public class HomeController : Controller
+	{
+		// GET: Home
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		// GET: AboutUs
+		public ActionResult AboutUs()
+		{
+			ViewBag.Title = "Acerca de nosotros";
+			ViewBag.Message = "Este es un mensaje creado por <code>HomeController::AboutUs()</code>.";
+			ViewBag.Tests = Test.GetTests();
+			return View();
+		}
+
+		// GET: Contact
+		public ActionResult Contact()
+		{
+			ViewBag.Title = "Contáctanos";
+			ViewBag.Message = "Este es un mensaje creado por <code>HomeController::Contact()</code>.";
+			return View();
+		}
+	}
+
+	public sealed class Test
+	{
+		public string Name { get; }
+		public string Url { get; }
+
+		private Test(string name, string url)
+		{
+			Name = name;
+			Url = url;
+		}
+
+		public static List<Test> GetTests()
+		{
+			return new List<Test> {
+				new Test("First Test - 1", "https://stackoverflow.com"),
+				new Test("Second Test - 2", null),
+				new Test("Third Test - 3", "https://stackoverflow.com"),
+				new Test("Fourth Test - 4", null),
+				new Test("Fifth Test - 5", "https://stackoverflow.com"),
+				new Test("Sixth Test - 6", null),
+				new Test("Seventh Test - 7", "https://stackoverflow.com"),
+				new Test("Eighth Test - 8", null),
+				new Test("Ninth Test - 9", "https://stackoverflow.com"),
+				new Test("Tenth Test - 10", null),
+			};
+		}
+	}
+}
